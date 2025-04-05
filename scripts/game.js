a45ba3ab4aab0e2bed4681f4e7b970d38890ba5e
Peter Januarius <pete@nexgencodecamp.com.au>
2025-04-05 19:13:40 +1100

    Added player and movement

--- scripts/game.js
+++ scripts/game.js
@@ -3,20 +3,26 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: '#000000',
-  scene: {
-    create() {
-      const text = this.add.text(400, 300, 'Hello, Phaser!', {
-        fontFamily: 'Arial',
-        fontSize: '64px',
-        color: '#ffffff'
-      });
-
-      // Center the origin of the text
-      text.setOrigin(0.5, 0.5);
-    }
-  }
+  scene: { preload, create, update },
 };
+
+let player, cursors;
+
+function preload() {
+  this.load.image('player', 'assets/images/player.png');
+}
+
+function create() {
+  player = this.add.sprite(400, 300, 'player');
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  if (cursors.left.isDown) player.x -=2;
+  if (cursors.right.isDown) player.x +=2;
+  if (cursors.up.isDown) player.y -=2;
+  if (cursors.down.isDown) player.y +=2;
+}
 
 new Phaser.Game(config);
 
-  
