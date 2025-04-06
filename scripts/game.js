8264f7bf9d2b6178a4e885903eb41ebcfb1ea9ae
Peter Januarius <pete@nexgencodecamp.com.au>
2025-04-06 16:11:46 +1000

    Added enemy

--- scripts/game.js
+++ scripts/game.js
@@ -6,15 +6,20 @@ const config = {
   scene: { preload, create, update },
 };
 
-let player, cursors;
+let player, cursors, enemy;
 
 function preload() {
   this.load.image('player', 'assets/images/player.png');
+  this.load.image('enemy', 'assets/images/enemy.png');
 }
 
 function create() {
   player = this.add.sprite(400, 300, 'player');
   cursors = this.input.keyboard.createCursorKeys();
+
+  this.time.delayedCall(1000, () => {
+    spawnEnemy(this);
+  });
 }
 
 function update() {
@@ -25,4 +30,14 @@ function update() {
 }
 
 new Phaser.Game(config);
+
+function spawnEnemy(scene) {
+  let x, y;
+  do {
+    x = Phaser.Math.Between(0, config.width);
+    y = Phaser.Math.Between(0, config.height);
+  } while (Math.abs(x - player.x) < 200 || Math.abs(y - player.y) < 200);
+
+  enemy = scene.add.sprite(x, y, 'enemy');
+}
 
